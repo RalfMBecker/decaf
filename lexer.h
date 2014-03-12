ed2c93cd5b5bd5c3124962fe399ef4f054a0dd4a
ralfmbecker <noonessleep@gmail.com>
2014-03-12 02:40:28 -0400

    minor layout

--- lexer.h
+++ lexer.h
@@ -1,30 +1,24 @@
 #ifndef LEXER_H_
 #define LEXER_H_
 
-//#include <string>
-
-/*
-void int double bool string class interface null this extends implements
-for while if else return break new NewArray Print ReadInteger ReadLine
-*/
 enum token{
-	// misc
-	tok_eof = -1, tok_return = -2,
-	// types
-	tok_void = -10, tok_int = -11, tok_double = -12, tok_bool = -13,
-	tok_string = -14, tok_null = -15,
-	// looping
-	tok_for = -20, tok_while = -21, tok_if = -22, tok_else = -23, 
-	tok_break = -24,
-	// i/o
-	tok_Print = -30, tok_ReadInteger = -31, tok_ReadLine = -32,
-	// classes
-	tok_class = -40, tok_interface = -41, tok_this = -42, tok_extends = -43,
-	tok_implements = -44, 
-	// allocation/declaration
-	tok_new = -50, tok_NewArray = -51,
-	// AST types
-	tok_ID = -100, tok_Int = -101, tok_flt = -102, tok_expr = -103,
+    // misc
+    tok_eof = -1, tok_return = -2,
+    // types
+    tok_void = -10, tok_int = -11, tok_double = -12, tok_bool = -13,
+    tok_string = -14, tok_null = -15,
+    // looping
+    tok_for = -20, tok_while = -21, tok_if = -22, tok_else = -23, 
+    tok_break = -24,
+    // i/o
+    tok_Print = -30, tok_ReadInteger = -31, tok_ReadLine = -32,
+    // classes
+    tok_class = -40, tok_interface = -41, tok_this = -42, tok_extends = -43,
+    tok_implements = -44,
+    // allocation/declaration
+    tok_new = -50, tok_NewArray = -51,
+    // AST types
+    tok_ID = -100, tok_Int = -101, tok_flt = -102, tok_expr = -103,
 };
 
 // it is more logical to make them extern here: they are defined in 
--- main.cpp
+++ main.cpp
@@ -7,43 +7,44 @@
 std::istream* input;
 
 int
-main(int argc, char* argv[]){
+main(int argc, char* argv[])
+{
 
-	switch(argc){
-	case 1: 
-		input = &std::cin; 
-		break;
-	case 2: 
-		input = new std::ifstream(argv[1]);
-		if (!input){  // WHY DOES ERROR NOT GET TRIGGERED??????? *********
-			std::cerr << argv[0] << ": can't open file " << argv[1];
-			exit(EXIT_FAILURE);
-		}
-		break;
-	default:
-		std::cerr << "Error: # of args\n"; // add basic error fct.  
-		break;
+    switch(argc){
+    case 1:
+	input = &std::cin;
+	break;
+    case 2: 
+	input = new std::ifstream(argv[1]);
+	if (!input){  // WHY DOES ERROR NOT GET TRIGGERED??????? *********
+	    std::cerr << argv[0] << ": can't open file " << argv[1];
+	    exit(EXIT_FAILURE);
 	}
+	break;
+    default:
+	std::cerr << "Error: # of args\n"; // add basic error fct.  
+	break;
+    }
 
-  // create ready-state
-  std::cout << "ready> ";
-	//  getNextToken();
+    // create ready-state
+    std::cout << "ready> ";
+    // getNextToken();
 
-	try{
-		while ( (EOF != getTok() ) )
-			;
-	}
+    try{
+	while ( (EOF != getTok() ) )
+	    ;
+    }
 
-	catch(Error& m){
-    m.print();
-  }
+    catch(Error& m){
+	m.print();
+    }
 
-  // enter driver, and let driver handle rest
-	/*
-  try{
-    throw Lexer_Error("test");
-  }
-	*/
+    // enter driver, and let driver handle rest
+    /*
+    try{
+	throw Lexer_Error("test");
+    }
+    */
 
-  return 0;
+    return 0;
 }
